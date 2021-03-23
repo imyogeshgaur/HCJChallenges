84a851cebb850ce50307c4996d15ca6d296dc52d
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-03-23 13:07:29 +0530

    Modal achieved

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
-const target = document.getElementById("target");
-const DropDown = document.querySelector(".outer_dropdown");
-
-target.addEventListener("click", () => {
-  DropDown.style.display = "block";
-  console.log("clicked");
-});
+class Modal {
+    constructor(el, options = {}) {
+      this.openModal = this.openModal.bind(this)
+      this.closeModal = this.closeModal.bind(this)
+      this.handleClick = this.handleClick.bind(this)
+  
+      this.modal = this.createModalNode(options)
+      el.addEventListener('click', this.openModal)
+    }
+  
+    openModal () {
+      document.body.appendChild(this.modal)
+    }
+  
+    closeModal () {
+      this.modal.remove()
+    }
+  
+    createModalNode (options) {
+      const { title, content } = options
+      const node = document.createElement('aside')
+      node.className = 'modal modal--open'
+      node.addEventListener('click', this.handleClick)
+      node.innerHTML =  `
+          <section class="modal__container">
+            <header class="modal__header">
+              <h2 class="modal__title">${title}</h2>
+            </header>
+            <section class="modal__content">
+              <p>${content}</p>
+            </section>
+            <footer class="modal__footer">
+              <button class="modal__btn-cancel">
+                Cancel
+              </button>
+              <button class="modal__btn-delete">
+                Delete
+              </button>
+            </footer>
+          </section>
+      `
+      return node
+    }
+  
+    handleClick (e) {
+      const closeClasses = [
+        'modal__btn-cancel',
+        'modal__btn-delete',
+        'modal modal--open',
+      ]
+      if (closeClasses.indexOf(e.target.className) > -1) {
+        this.closeModal()
+      }
+    }
+  }
+  
+  const modal = new Modal(document.getElementById('delete'), {
+    title: 'This is a new custom title',
+    content: 'Any content can go here',
+  })
